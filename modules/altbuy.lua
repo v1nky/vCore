f4469500c3b7c48d5019fedde9d8174e43544d10
v1nky <v1nk@me.com>
2019-01-11 20:35:00 +0000

    v2.1
    fixed: altbuy.lua error when hovering over item (thanks Craig)

--- modules/altbuy.lua
+++ modules/altbuy.lua
@@ -7,7 +7,6 @@ function vCore:AltBuy()
     local NEW_ITEM_VENDOR_STACK_BUY = ITEM_VENDOR_STACK_BUY
     ITEM_VENDOR_STACK_BUY = "|cffa9ff00"..NEW_ITEM_VENDOR_STACK_BUY.."|r"
 
-        -- Alt-click to buy a stack.
     hooksecurefunc("MerchantItemButton_OnModifiedClick", function(self, ...)
         if not vCoreDB.AltBuy then return end
         if IsAltKeyDown() then
@@ -22,49 +21,8 @@ function vCore:AltBuy()
                 BuyMerchantItem(self:GetID(), GetMerchantItemMaxStack(self:GetID()))
             end
         end
-
-        -- auto repair
-        local g = CreateFrame("Frame")
-        g:RegisterEvent("MERCHANT_SHOW")
-        g:SetScript("OnEvent", function()
-            local bag, slot
-            for bag = 0, 4 do
-                    for slot = 0, GetContainerNumSlots(bag) do
-                            local link = GetContainerItemLink(bag, slot)
-                            if link and (select(3, GetItemInfo(link)) == 0) then
-                                    UseContainerItem(bag, slot)
-                            end
-                    end
-            end
-
-            if(CanMerchantRepair()) then
-                    local cost = GetRepairAllCost()
-                    if cost > 0 then
-                            local money = GetMoney()
-                            if IsInGuild() then
-                                    local guildMoney = GetGuildBankWithdrawMoney()
-                                    if guildMoney > GetGuildBankMoney() then
-                                            guildMoney = GetGuildBankMoney()
-                                    end
-                                    if guildMoney > cost and CanGuildBankRepair() then
-                                            RepairAllItems(1)
-                                            print(format("|cfff07100Repair cost covered by G-Bank: %.1fg|r", cost * 0.0001))
-                                            return
-                                    end
-                            end
-                            if money > cost then
-                                    RepairAllItems()
-                                    print(format("|cffead000Repair cost: %.1fg|r", cost * 0.0001))
-                            else
-                                    print("Not enough gold to cover the repair cost.")
-                            end
-                    end
-            end
-        end)
-
     end)
 
-        -- Add a hint to the tooltip.
     local function IsMerchantButtonOver()
         return GetMouseFocus():GetName() and GetMouseFocus():GetName():find("MerchantItem%d")
     end
@@ -73,7 +31,8 @@ function vCore:AltBuy()
         if not vCoreDB.AltBuy then return end
         if MerchantFrame:IsShown() and IsMerchantButtonOver() then
             for i = 2, GameTooltip:NumLines() do
-                if _G["GameTooltipTextLeft"..i]:GetText():find("<[sS]hift") then
+                local line = _G["GameTooltipTextLeft"..i]:GetText() or ""
+                if line:find("<[sS]hift") then
                     GameTooltip:AddLine("|cff00ffcc"..L.AltBuyVendorToolip.."|r")
                 end
             end
