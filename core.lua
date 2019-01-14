54a238502988a445a2b17ed88f8a0bc7c9d31013
v1nky <v1nk@me.com>
2019-01-14 00:43:29 +0000

    Update core.lua
    removed cvar from core

--- core.lua
+++ core.lua
@@ -14,7 +14,6 @@ function vCore_OnEvent(self, event, ...)
     if event == "PLAYER_LOGIN" then
 		SetCVar("ScreenshotQuality", 10)
 		SetCVar("floatingCombatTextCombatState", 1)
-		SetCVar("showHonorAsExperience", 0)
     elseif event == "ADDON_LOADED" then
         local name = ...
         if name == "vCore" then
